d143fa64b3fc9755dacf5e09e0eca9b3b52cc6a8
ahmed ali Soomro <74205298+ahmedali762@users.noreply.github.com>
2021-12-15 22:28:58 +0500

    Add files via upload

---
+++ src/screens/MainHome.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+
+
+export default function MainHome() {
+  return (<>
+    <div className="bg">
+      
+    <div className='appbar'>
+     <a href="#">Home</a>
+     <a href="#">Contacts</a>
+     <a href="#">About</a>
+      
+      </div>
+      <div className="txthead">
+        <h1>Emerald Inn.</h1>
+        <h2>Welcome To Emerald</h2>
+        <p>Hotel & Resorts</p>
+      </div>
+    </div>
+    <div className="rooms">
+      
+    </div>
+
+  
+  </>
+  )
+}
